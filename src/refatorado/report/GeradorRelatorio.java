cf76eacc8be30c8953d45ca22959fcd70d2dc1db
Thiago-Sampaio-Santos <139243107+Thiago-Sampaio-Santos@users.noreply.github.com>
2025-08-21 15:26:34 -0300

    Adcionando melhorias

--- src/refatorado/report/GeradorRelatorio.java
+++ src/refatorado/report/GeradorRelatorio.java
@@ -4,9 +4,10 @@ import refatorado.model.*;
 
 public class GeradorRelatorio {
     public static void gerar(SensorTemperatura temp, SensorPresenca pres, SensorLuminosidade luz) {
-        System.out.println("Relatório:");
-        System.out.println("Temperatura: " + temp.getTemperatura());
-        System.out.println("Presença: " + pres.isPresenca());
-        System.out.println("Luminosidade: " + luz.getLuminosidade());
+        System.out.println("\n========== RELATÓRIO DO MONITORAMENTO ==========\n");
+        System.out.println(" Temperatura : " + temp.getTemperatura());
+        System.out.printf(" Presença    : %s%n", pres.isPresenca() ? "Detectada" : "Não detectada");
+
+        System.out.println(" Luminosidade: " + luz.getLuminosidade());
     }
 }
--- src/refatorado/utils/ColetorDadosSensor.java
+++ src/refatorado/utils/ColetorDadosSensor.java
@@ -1,5 +1,10 @@
 package refatorado.utils;
 
+import refatorado.model.Sensor;
+
 public class ColetorDadosSensor {
-    
+    public static void coletar(Sensor sensor) {
+        System.out.println("Coletando dados do sensor: " + sensor.getClass().getSimpleName());
+        sensor.coletarDados();
+    }
 }
